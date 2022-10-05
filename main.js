b1154e31660715beb87e435b9fe4ff7a84103c42
mike.lythgoe <git@mikelythgoe.net>
2022-10-05 12:37:24 +0100

    added description to the data and used it to display in a paragraph

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            product: 'Socks',
+            description: 'Nice & fluffy for the winter'
         }
     }
 })
+
+// the options object is passed into the createApp function - as an anonymous object.
+// It allows us to pass in some properties to configure our app.
+// We can pass in optional data but the options object itself is NOT optional, it must be passed in, even if it's empty.
+// We then use 'mountedApp' in index.html, mounting the app onto the DOM, which plugs into the div with the id of 'app',
+// which we supply the name of when we create the app
+// After that, everything within the 'app' div has access to everything in the vue app
+// We can see above that we have passed in the data function
+
+// Vue is reactive, so when the data changes, the UI accessing the data is also changed.
